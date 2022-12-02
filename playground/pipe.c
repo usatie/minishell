d7cca3e4a51759eae00cb76d31085bba3d1a6e2a
hatopopo <94654563+hatopp@users.noreply.github.com>
2022-12-02 15:08:14 +0900

    playground pipe.c

--- playground/pipe.c
+++ playground/pipe.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
 
@@ -6,5 +7,22 @@ int	main(void)
 	int fd[2];
 
 	int err = pipe(fd);
-	printf("fd[0] = %d fd[1] = %d ,err = %d", fd[0], fd[1], err);
+
+	printf("fd[0] = %d fd[1] = %d ,err = %d\n\n", fd[0], fd[1], err);
+
+	int ret = write(fd[0], "hello", 5);
+	printf("fd[0] : %d errno : %d\n", ret, errno);
+	ret = write(fd[1], "world", 5);
+	printf("fd[1] : %d\n", ret);
+	errno = 0;
+
+	char buf[100];
+
+	ret = read(fd[0], buf, 99);
+	buf[ret] = '\0';
+	printf("fd[0] : %d errno : %d\n", ret, errno);
+	printf("buf : %s\n", buf);
+
+	// ret = read(fd[1], buf, 99);
+	// printf("fd[1] : %d errn : %d\n", ret, errno);
 }
