d6f2187193b848c2a7b13ca5b1ceeeb0feb9f32d
hatopopo <94654563+hatopp@users.noreply.github.com>
2022-12-04 11:19:56 +0900

    playground APUE cpywait.c using two fork()

--- playground/APUE/cpywait.c
+++ playground/APUE/cpywait.c
@@ -1,4 +1,10 @@
 #include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#include "apue.h"
+
 
 int main()
 {
@@ -7,4 +13,22 @@ int main()
     if((pid = fork()) < 0){
         err_sys("fork error");
     }
+    else if(pid == 0){
+        if ((pid = fork()) < 0)
+            err_sys("fork error");
+        else if (pid > 0)
+            exit(0);
+
+        // 上の文で実の親がexit()を呼ぶと、init が親になる？？？
+        // >> init は全てのプロセスの親である。 "/etc/inittab"
+
+        sleep(2);
+        printf("second child, parent pid = %ld\n", (long)getppid()); //　= 最初の子
+        exit(0);
+    }
+
+    if (waitpid(pid, NULL, 0) != pid)
+        err_sys("waitpid error");
+
+    exit(0);
 }
