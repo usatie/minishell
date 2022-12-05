cda156a0c72cc220cccf6a2a6de8131985cb4784
hatopopo <94654563+hatopp@users.noreply.github.com>
2022-12-05 13:32:04 +0900

    playground APUE WIP do_exec.c

---
+++ playground/APUE/cpy/do_exec.c
@@ -0,0 +1,24 @@
+#include <sys/wait.h>
+#include "../apue.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main(){
+    pid_t pid;
+
+
+    if ((pid = fork()) < 0){
+        err_sys("fork_error");
+    }
+
+    else if (pid == 0){
+        if (execl("/home/sar/bin/testinterp", "testinterp", "myarg1", "MYARG2", (char *)0) < 0){
+            err_sys("execel error");
+        }
+    }
+    if (waitpid(pid, NULL, 0) < 0)
+        err_sys("waitpid error");
+    exit(0);
+}
+
