db93408473cf5939c5e69416df4b87f75cd845d4
hatopopo <94654563+hatopp@users.noreply.github.com>
2022-12-06 04:06:34 +0900

    :sparkles: Add line0106.c

---
+++ playground/POINTA/chap01/list0106.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("The size of char is [%u]bite.\n", (unsigned)sizeof(char));
+	printf("The size of  int is [%u]bite.\n", (unsigned)sizeof(int));
+	printf("The size of long is [%u]bite.\n", (unsigned)sizeof(long));
+
+	return 0;
+}
