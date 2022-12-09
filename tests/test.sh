6957835c3566f83b0579d30366cf88a2b7d1646c
Shun Usami <usatie@gmail.com>
2022-12-10 00:28:47 +0900

    :recycle: Refactor test script

--- tests/test.sh
+++ tests/test.sh
@@ -5,16 +5,20 @@ cleanup() {
 	rm -f "$1".cmp "$1".out
 }
 
-assert_output() {
+assert() {
 	printf "%-30s:" "[$1]"
 
 	echo -e "$1" | ./minishell >out 2>/dev/null
 	actual=$?
-	mv "$2" "$2".out
+	if [ $# -eq 2 ]; then
+		mv "$2" "$2".out
+	fi
 
 	echo -e "$1" | bash >cmp 2>/dev/null
 	expected=$?
-	mv "$2" "$2".cmp
+	if [ $# -eq 2 ]; then
+		mv "$2" "$2".cmp
+	fi
 
 	diff out cmp && echo -n "diff OK" || terminate=1
 	if [ "$terminate" = "1" ]; then
@@ -32,39 +36,18 @@ assert_output() {
 		exit
 	fi
 
-	diff "$2".out "$2".cmp && echo ", diff $2 OK" || terminate=1
-	if [ "$terminate" = "1" ]; then
-		echo ", diff $2 NG"
-		cleanup $2
-		exit
+	if [ $# -eq 2 ]; then
+		diff "$2".out "$2".cmp && echo ", diff $2 OK" || terminate=1
+		if [ "$terminate" = "1" ]; then
+			echo ", diff $2 NG"
+			cleanup $2
+			exit
+		fi
+	else
+		echo
 	fi
 
 	cleanup $2
-}
-
-assert() {
-	printf "%-30s:" "[$1]"
-	echo -e "$1" | ./minishell >out 2>/dev/null
-	actual=$?
-	echo -e "$1" | bash >cmp 2>/dev/null
-	expected=$?
-	diff out cmp && echo -n "diff OK" || terminate=1
-	if [ "$terminate" = "1" ]; then
-		echo "diff NG"
-		cleanup
-		exit
-	fi
-
-	if [ "$actual" = "$expected" ]; then
-		echo ", status OK"
-	else
-		echo ", status NG. Expected $expected, actual is $actual"
-		terminate=1
-		cleanup
-		exit
-	fi
-
-	cleanup
 }
 
 assert ""
@@ -84,9 +67,9 @@ assert "echo \"'hello'\""
 assert "     pwd   "
 assert "echo hello'world'\"42\""
 assert 'ec"ho" he"ll"o'
-assert_output 'echo hello >hello.txt' 'hello.txt'
-assert_output 'echo hello 1>hello.txt' 'hello.txt'
-assert_output 'invalid 2>hello.txt' 'hello.txt'
+assert 'echo hello >hello.txt' 'hello.txt'
+assert 'echo hello 1>hello.txt' 'hello.txt'
+assert 'invalid 2>hello.txt' 'hello.txt'
 # assert "ls -l"  # This test fails because out and cmp affects the output of `ls`
 
 echo "OK :D"
