6c5ae9d04377f60a57b8f20d85524e5f9e7590b8
hatopopo <94654563+hatopp@users.noreply.github.com>
2022-12-12 20:38:44 +0900

    :recycle: Rewrite test.sh using assert()

--- hato01/test.sh
+++ hato01/test.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
+#!/bin/bash -e
 
-echo "exit" | ./minishell
-echo "hello" | ./minishell
-echo "exit success"
+
+assert() {
+	echo -n "$1:"
+	echo "$1" | ./minishell > out
+	echo "$1" | bash > cmp
+	diff out cmp
+	echo "OK"
+}
+
+assert "exit"
+assert "pwd"
