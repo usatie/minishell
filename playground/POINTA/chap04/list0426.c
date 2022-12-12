8651f406c4c96b268a9cfa2edfbab8b92ffc6caf
hatopopo <94654563+hatopp@users.noreply.github.com>
2022-12-12 17:48:00 +0900

    :speakles: Add list0426.c

--- playground/POINTA/chap04/list0426.c
+++ playground/POINTA/chap04/list0426.c
@@ -6,11 +6,21 @@ char *strncpy(char *s1, const char *s2, size_t n)
 
     while (n) {
         n--;
-        if (!(*s1++ = *s2++) break);
+        if (!(*s1++ = *s2++)) break;
     }
 
     while (n--)
         *s1++ = '\0';
 
     return p;
+}
+
+int main(void)
+{
+    const char *s1 = "123";
+    char s2[128];
+    char *ret = strncpy(s2, s1, 5);
+
+    printf("s1 = %s, s2 = %s, ret = %s\n", s1, s2, ret);
+    return 0;
 }
