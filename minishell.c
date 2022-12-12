2d063e83173a9844bbef0685c25bb10fb81b9dac
hatopopo <94654563+hatopp@users.noreply.github.com>
2022-12-12 13:30:28 +0900

    :spakrles: Separate buildin or not

--- minishell.c
+++ minishell.c
@@ -2,14 +2,52 @@
 #include <readline/readline.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
 
 extern char     *envp[];
 
-void    exec_cmd(const char *line)
+#define NUM_BUILTIN 7
+
+bool is_builtin(const char *cmd)
+{
+    const char *cmds[NUM_BUILTIN] = {"echo", "cd", "pwd", "export", "unset", "env", "exit"};
+    int i = 0;
+
+    while (i < NUM_BUILTIN)
+    {
+        if (strcmp(cmd, cmds[i]) == 0)
+            return (true);
+        i++;
+    }
+    return (false);
+}
+
+void exec_builtin(const char *line)
+{
+    if (strcmp(line, "exit") == 0)
+        exit(0);
+    // TODO:echo
+    // TODO:cd
+    // TODO:pwd
+    // TODO:export
+    // TODO:unset
+    // TODO:env
+    system(line);
+}
+
+void exec_non_builtin(const char *line)
 {
     system(line);
 }
+
+void exec(const char *line)
+{
+    if (is_builtin(line))
+        exec_builtin(line);
+    else
+        exec_non_builtin(line);
+}
+
 
 int main()
 {
@@ -21,12 +59,7 @@ int main()
         if (line == NULL) 
             break;
         if (*line)
-        {
-            if (strcmp(line, "exit") == 0)
-                exit(0);
-//           printf("%s\n", line);
-            exec_cmd(line);
-       }
+            exec(line);
     }
     return 0;
 }
