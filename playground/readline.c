1eafbcabeb6ec801d74c752a7a94d2900ca2ac1f
hatopopo <94654563+hatopp@users.noreply.github.com>
2022-12-12 12:48:04 +0900

    :sparkles: Play readline()

--- playground/readline.c
+++ playground/readline.c
@@ -1,24 +1,12 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   readline.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mkunimot <hatopopo142@gmail.com>           +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/01 14:23:43 by mkunimot          #+#    #+#             */
-/*   Updated: 2022/12/01 14:26:40 by mkunimot         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include <readline/readline.h>
-#include <stdio.h>
-
-int	main(void)
+# include <stdio.h>
+# include <readline/readline.h>
+int main(void)
 {
-	char	*hoge;
-	char	*line;
+    char *hoge;
+    char *line;
 
-	hoge = "minishell $ ";
-	line = readline(hoge);
-	printf("%s\n", line);
+    hoge = "minishell $ ";
+    line = readline(hoge);
+    printf("%s\n", line);
 }
+
