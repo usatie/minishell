519644faeeedb421f3688e07c3837134958c6429
Shun Usami <usatie@gmail.com>
2022-12-13 18:25:00 +0900

    :sparkles: Playground signal.c

---
+++ playground/signal.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <signal.h>
+#include <stdio.h>
+#include <readline/readline.h>
+
+// gcc signal.c -lreadline
+
+pid_t	childpid;
+
+void	handler(int signum)
+{
+	(void)signum;
+	printf("\n");
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+	kill(childpid, SIGINT);
+}
+
+int	main(void)
+{
+	char	*line;
+
+	signal(SIGINT, handler);
+	while (1)
+	{
+		line = readline("nanoshell$ ");
+		system(line);
+	}
+	return (0);
+}
