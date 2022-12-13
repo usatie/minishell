defac8450b40b87bda544e89488e27c8d0b196ef
hatopopo <94654563+hatopp@users.noreply.github.com>
2022-12-13 12:50:44 +0900

    :sparkles: Add list0437-39.c

--- playground/POINTA/chap04/list0437.c
+++ playground/POINTA/chap04/list0437.c
@@ -12,11 +12,58 @@ char *strpbrk(const char *s1, const char *s2)
     return NULL;
 }
 
+/*
 int main()
 {
-    const char *s1 = "RADIANCE";
-    const char *s2 = "ABCD";
+    const char *s1 = "hello '$PATH' world";
+    const char *s2 = " $'";
+    char *s = s1;
 
-    printf("%s\n", strpbrk(s1, s2));
+    printf("%s\n", s);
+    while (s = strpbrk(s, s2)){
+        printf("%s\n", s);
+        s++;
+    }
+
+    return 0;
+}
+*/
+
+// list0438.c
+/*
+int main(void)
+{
+    char s1[128], s2[128];
+    char *p;
+
+    printf("文字列s1:");  scanf("%s", s1);
+    printf("文字列s2:");  scanf("%s", s2);
+
+    if ((p = strpbrk(s1, s2)) == NULL)
+        printf("文字列s2中のどの文字も文字列s1中には存在しません\n");
+    else
+        printf("文字列s2中に含まれる文字の文字列s1での最初の出現以降は\"%s\"です\n", p);
+    return 0;
+}
+*/
+
+// list0439.c
+int main (void)
+{
+    char str[128];
+    char ultr[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char *p;
+
+    printf("文字列を入力してください:");
+    scanf("%s", str);
+
+    puts("その文字列に含まれる英大文字は以下の通りです");
+
+    p = str;
+
+    while ((p = strpbrk(p, ultr)) != NULL)
+        putchar(*p++);
+    putchar('\n');
+
     return 0;
 }
