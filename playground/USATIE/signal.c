f70545c0ed3f004001fa9cc635abb0bb8c3538c3
hatopopo <94654563+hatopp@users.noreply.github.com>
2022-12-20 15:40:58 +0900

    :construction: WIP todo:know about rl_()

---
+++ playground/USATIE/signal.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <signal.h>
+#include <stdio.h>
+#include <readline/readline.h>
+
+//gcc -L $RL_PATH/lib -I $RL_PATH/include signal.c -lreadline
+
+void handler(int signum)
+{
+	(void)signum;
+	printf("\n");
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+int main(void)
+{
+	char *line;	
+
+	//signal(SIGINT, handler);
+	while (1)
+	{
+		line = readline("nanoshell $ ");
+		system(line);
+	}
+	return (0);
+}
