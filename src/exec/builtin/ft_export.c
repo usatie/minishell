287b5068948e07e723438fdb94602ef0c25d6921
Shun Usami <usatie@gmail.com>
2022-12-21 19:11:33 +0900

    :bug: Fix export, should not be freed

--- src/exec/builtin/ft_export.c
+++ src/exec/builtin/ft_export.c
@@ -6,16 +6,29 @@
 /*   By: susami <susami@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 09:25:42 by susami            #+#    #+#             */
-/*   Updated: 2022/12/16 09:25:43 by susami           ###   ########.fr       */
+/*   Updated: 2022/12/21 19:11:27 by susami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "minishell.h"
 
+// assignment should not be freed.
+// So it must be allocated and dismissed here.
 int	ft_export(char **argv)
 {
-	if (putenv(argv[1]) < 0)
-		return (1);
+	char	*assignment;
+	int		i;
+
+	i = 1;
+	while (argv[i])
+	{
+		assignment = strdup(argv[i]);
+		if (!assignment)
+			fatal_exit("strdup");
+		if (putenv(assignment) < 0)
+			return (1);
+		i++;
+	}
 	return (0);
 }
