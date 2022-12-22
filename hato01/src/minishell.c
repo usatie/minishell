e61b69fe0b3fa38fdf754b5fd4647cc1388d47ab
hatopopo <94654563+hatopp@users.noreply.github.com>
2022-12-22 14:51:21 +0900

    :sparkles: Add Makefile and did a lot

--- hato01/src/minishell.c
+++ hato01/src/minishell.c
@@ -1,86 +1,117 @@
-#include <stdio.h>
-#include <readline/readline.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <unistd.h>
 #include <errno.h>
+#include <readline/readline.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
 
-extern char     **environ;
-int             status;
+//#include "minishell.h"
 
-#define NUM_BUILTIN 7
+#define PROMPT "minishell > "
 
-bool is_builtin(const char *cmd)
+typedef struct s_token	t_token;
+
+struct s_token {
+	char	*pos;
+	size_t	len;
+	t_token	*next;
+};
+
+t_token *new_token(char *pos, size_t len)
 {
-    const char *cmds[NUM_BUILTIN] = {"echo","cd","pwd","export","unset","env","exit"};
-    int i = 0;
+	t_token	*tok;
 
-    while (i < NUM_BUILTIN)
-    {
-        if (strcmp(cmd, cmds[i]) == 0)
-            return (true);
-        i++;
-    }
-    return (false);
+	tok = calloc(sizeof(t_token), 1);
+	tok->pos = pos;
+	tok->len = len;
+	return (tok);
 }
 
-int mysystem(const char *cmd)
+char *find_path(char *cmd)
 {
-    int     st;
-    char *argv[4] = {"/bin/sh", "-c", (char *)cmd, NULL};
+	char *path;
 
-    switch (fork()) {
-        case -1:
-            exit(1);
-        case 0:
-            execve("/bin/sh", argv, environ);
-            exit(1);
-        default:
-            break;
-    }
-    wait(&st);
-    status = st;
-    return (st);
+	path = malloc(PATH_MAX);
+	path[0] = '\0';
+	if (cmd[0] != '/')
+		strcpy(path, "/bin/");
+	strcat(path, cmd);
+	if (access(path, X_OK) == 0)
+		return (path);
+	free(path);
+	return (NULL);
 }
 
-void    exec_builtin(const char *line)
+t_token *tokenize(char *line)
 {
-    if (strcmp(line, "exit") == 0)
-            exit(0);
-    // TODO: echo
-    // TODO: cd
-    // TODO: pwd
-    // TODO: export
-    // TODO: unset
-    // TODO: env
-    status = mysystem(line);
+	t_token	*tok;
+	size_t	len;
+
+	while (*line == ' ')
+		line++;
+	len = 0;
+	while (line[len] && line[len] != ' ')
+		len++;
+	if (len == 0)
+		return (NULL);
+	tok = new_token(line, len);
+	tok->next = tokenize(line + len);
+	return (tok);
 }
 
-void exec_non_builtin(const char *line)
+int ft_system(char *cmd)
 {
-    status = system(line);
-}
+	extern char		**environ;
+	int				status;
+	pid_t			child_pid;
+	t_token			*tok;
+	char			*argv[100];
+	char			*path;
+	size_t			i;
 
-void    exec(const char *line)
-{
-    if (is_builtin(line))
-        exec_builtin(line);
-    else
-        exec_non_builtin(line);
+	tok = tokenize(cmd);
+	if (tok == NULL)
+		return 0;
+	path = find_path(strndup(tok->pos, tok->len));
+	if (path == NULL)
+		return (127 << 8);
+	i = 0;
+	while (tok)
+	{
+		argv[i] = strndup(tok->pos, tok->len);
+		i++;
+		tok = tok->next;
+	}
+	argv[i] = NULL;
+	child_pid = fork();
+	if (child_pid < 0)
+		return (-1);
+	else if (child_pid == 0)
+	{
+		execve(path, argv, environ);
+		exit(127);
+	}
+	if (waitpid(child_pid, &status, 0) < 0)
+		return (-1);
+	else
+		return (status);
 }
 
 int main(void)
 {
-    char *line;
+	char	*line;
+	int		status;
 
-    while (1)
-    {
-        line = readline("minishell $ ");
-        if (line == NULL)
-            break;
-        if (*line)
-            exec(line);
-    }
-    return (status >> 8);
+	while (1)
+	{
+		line = readline(PROMPT);
+		if (line == NULL)
+			break;
+		if (strcmp(line, "exit") == 0)
+			exit(1);
+		status = ft_system(line);
+		free(line);
+	}
+	return (WEXITSTATUS(status));
 }
