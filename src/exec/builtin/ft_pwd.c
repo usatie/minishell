4452cab83c209a0cfd0e321fe9fe03ad89a0d05b
Shun Usami <usatie@gmail.com>
2022-12-22 14:10:28 +0900

    :recycle: Refactor ft_pwd.c TOO_MANY_LINES

--- src/exec/builtin/ft_pwd.c
+++ src/exec/builtin/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: susami <susami@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 09:26:48 by susami            #+#    #+#             */
-/*   Updated: 2022/12/16 11:05:25 by susami           ###   ########.fr       */
+/*   Updated: 2022/12/22 14:09:59 by susami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,19 @@
 #include "minishell.h"
 #include "libft.h"
 
+static bool	is_pwd_valid(char *pwd);
+
 int	ft_pwd(char *argv[])
 {
 	char		cwd[PATH_MAX];
-	struct stat	st1;
-	struct stat	st2;
 	char		*pwd;
 
 	(void)argv;
 	pwd = getenv("PWD");
-	if (pwd)
+	if (pwd && is_pwd_valid(pwd))
 	{
-		bzero(&st1, sizeof(struct stat));
-		bzero(&st2, sizeof(struct stat));
-		stat(".", &st1);
-		stat(pwd, &st2);
-		if (st1.st_ino == st2.st_ino)
-		{
-			ft_putendl_fd(pwd, STDOUT_FILENO);
-			return (0);
-		}
+		ft_putendl_fd(pwd, STDOUT_FILENO);
+		return (0);
 	}
 	if (getcwd(cwd, PATH_MAX))
 	{
@@ -44,4 +37,18 @@ int	ft_pwd(char *argv[])
 	}
 	perror("pwd");
 	return (1);
+}
+
+static bool	is_pwd_valid(char *pwd)
+{
+	struct stat	st1;
+	struct stat	st2;
+
+	bzero(&st1, sizeof(struct stat));
+	bzero(&st2, sizeof(struct stat));
+	stat(".", &st1);
+	stat(pwd, &st2);
+	if (st1.st_ino == st2.st_ino)
+		return (true);
+	return (false);
 }
