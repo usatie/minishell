fd57e4dd69038a724de7ff37d11dbf917c3ee5b1
Shun Usami <usatie@gmail.com>
2022-12-22 15:04:50 +0900

    :recycle: Refactor tokenize.c TOO_MANY_LINES

--- src/tokenize/tokenize.c
+++ src/tokenize/tokenize.c
@@ -6,7 +6,7 @@
 /*   By: susami <susami@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 10:25:51 by susami            #+#    #+#             */
-/*   Updated: 2022/12/22 14:33:58 by susami           ###   ########.fr       */
+/*   Updated: 2022/12/22 15:04:30 by susami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 static bool		consume_blank(char **rest, char *line);
 static t_token	*string(char **rest, char *line);
+static t_token	*number(char **rest, char *line);
 
 // "cat -e Makefile"
 //  ^   ^  ^
@@ -32,41 +33,22 @@ t_token	*tokenize(char *line)
 	cur = &head;
 	while (*line)
 	{
-		// blank
 		if (consume_blank(&line, line))
 			continue ;
 		// Multi character punctuator
-		if (startswith(line, "<<") || startswith(line, ">>"))
+		else if (startswith(line, "<<") || startswith(line, ">>"))
 		{
 			cur->next = new_token(line, 2, TK_PUNCT);
-			cur = cur->next;
 			line += 2;
-			continue ;
 		}
 		// Single character punctuator
-		if (strchr("|<>", *line) != NULL)
-		{
-			cur->next = new_token(line, 1, TK_PUNCT);
-			cur = cur->next;
-			line++;
-			continue ;
-		}
-		// Number
-		if (is_number(line))
-		{
-			cur->next = new_token(line, 0, TK_NUM);
-			cur = cur->next;
-			cur->val = strtol(line, &line, 10);
-			cur->len = line - cur->pos;
-			cur->str = new_str(STR_PLAIN, cur->pos, cur->len, NULL);
-			continue ;
-		}
-		// String
-		{
+		else if (strchr("|<>", *line) != NULL)
+			cur->next = new_token(line++, 1, TK_PUNCT);
+		else if (is_number(line))
+			cur->next = number(&line, line);
+		else
 			cur->next = string(&line, line);
-			cur = cur->next;
-			continue ;
-		}
+		cur = cur->next;
 	}
 	cur->next = new_token(line, 0, TK_EOF);
 	return (head.next);
@@ -84,7 +66,19 @@ static bool	consume_blank(char **rest, char *line)
 	return (false);
 }
 
-t_token	*string(char **rest, char *line)
+static t_token	*number(char **rest, char *line)
+{
+	t_token	*tok;
+
+	tok = new_token(line, 0, TK_NUM);
+	tok->val = strtol(line, &line, 10);
+	tok->len = line - tok->pos;
+	tok->str = new_str(STR_PLAIN, tok->pos, tok->len, NULL);
+	*rest = line;
+	return (tok);
+}
+
+static t_token	*string(char **rest, char *line)
 {
 	t_token	*tok;
 	t_str	head;
@@ -94,46 +88,18 @@ t_token	*string(char **rest, char *line)
 	cur = &head;
 	while (*line && !isspace(*line))
 	{
-		// Single Quotes
 		if (*line == '\'')
-		{
 			cur->next = single_quotes(&line, line);
-			cur = cur->next;
-			tok->len += cur->len;
-			continue ;
-		}
-		// Double Quotes
 		else if (*line == '"')
-		{
 			cur->next = double_quotes(&line, line);
-			cur = cur->next;
-			tok->len += cur->len;
-			continue ;
-		}
-		// Special Parameter
 		else if (is_special_param(line))
-		{
 			cur->next = special_parameter(&line, line);
-			cur = cur->next;
-			tok->len += cur->len;
-			continue ;
-		}
-		// Parameter
 		else if (is_variable(line))
-		{
 			cur->next = variable(&line, line);
-			cur = cur->next;
-			tok->len += cur->len;
-			continue ;
-		}
-		// Plain text
 		else
-		{
 			cur->next = plain_text(&line, line);
-			cur = cur->next;
-			tok->len += cur->len;
-			continue ;
-		}
+		cur = cur->next;
+		tok->len += cur->len;
 	}
 	tok->str = head.next;
 	*rest = line;
