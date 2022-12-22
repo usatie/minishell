c117bf0edcd52416657edd15ca554b6f7d2b8fc0
Shun Usami <usatie@gmail.com>
2022-12-22 16:53:15 +0900

    :sparkles: Builtin export without arguments

--- src/exec/builtin/ft_export.c
+++ src/exec/builtin/ft_export.c
@@ -6,16 +6,31 @@
 /*   By: susami <susami@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 09:25:42 by susami            #+#    #+#             */
-/*   Updated: 2022/12/21 19:11:27 by susami           ###   ########.fr       */
+/*   Updated: 2022/12/22 16:43:55 by susami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
 #include "minishell.h"
 
 // assignment should not be freed.
 // So it must be allocated and dismissed here.
-int	ft_export(char **argv)
+static int	print_all_variables(void)
+{
+	int	i;
+
+	i = 0;
+	while (environ[i])
+	{
+		ft_putstr_fd("declare -x ", STDOUT_FILENO);
+		ft_putendl_fd(environ[i], STDOUT_FILENO);
+		i++;
+	}
+	return (0);
+}
+
+static int	putenv_all(char **argv)
 {
 	char	*assignment;
 	int		i;
@@ -31,4 +46,12 @@ int	ft_export(char **argv)
 		i++;
 	}
 	return (0);
+}
+
+int	ft_export(char **argv)
+{
+	if (argv[1] == NULL)
+		return (print_all_variables());
+	else
+		return (putenv_all(argv));
 }
