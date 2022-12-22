51ec78ad12e3c9fc339dc45d40b423700f02b46c
Shun Usami <usatie@gmail.com>
2022-12-22 16:53:35 +0900

    :sparkles: Builtin exit too many arguments

--- src/exec/builtin/ft_exit.c
+++ src/exec/builtin/ft_exit.c
@@ -6,20 +6,23 @@
 /*   By: susami <susami@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 09:34:06 by susami            #+#    #+#             */
-/*   Updated: 2022/12/16 13:41:58 by susami           ###   ########.fr       */
+/*   Updated: 2022/12/22 16:52:49 by susami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "minishell.h"
+#include "libft.h"
 
 int	ft_exit(char **argv)
 {
 	char	*arg;
 
 	arg = argv[1];
-	if (arg)
-		exit(atoi(arg));
-	else
+	if (!arg)
 		exit(g_env.status);
+	if (argv[2] == NULL)
+		exit(atoi(arg));
+	ft_putendl_fd("exit: too many arguments", STDERR_FILENO);
+	return (1);
 }
