2bb8f66c68958353e3639d4a39ca0bf4663e1498
Shun Usami <usatie@gmail.com>
2022-12-29 15:54:02 +0900

    :recycle: Fix `echo | |` as syntax error
    - Fix #36

--- src/parse/simple_command.c
+++ src/parse/simple_command.c
@@ -6,41 +6,28 @@
 /*   By: susami <susami@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 22:21:26 by susami            #+#    #+#             */
-/*   Updated: 2022/12/22 22:23:46 by susami           ###   ########.fr       */
+/*   Updated: 2022/12/29 15:53:32 by susami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static bool	is_redirection(const t_token *tok);
+bool			is_control_op(t_token *tok);
+static bool		is_redirection(const t_token *tok);
+static t_node	*simple_command_element(t_token **rest, t_token *tok);
+static void		append_element(t_node *cmd, t_node *elem);
 
-// simple_command = (redirection | word)*
+// simple_command is terminated by control operator
+// simple_command         = simple_command_element
+//                        | simple_command_element simple_command
 t_node	*simple_command(t_token **rest, t_token *tok)
 {
 	t_node	*cmd;
 
 	cmd = new_node(ND_CMD, tok);
-	while (!at_eof(tok) && !equal(tok, "|"))
-	{
-		if (is_redirection(tok))
-			cmd->redirects = add_node_back(cmd->redirects, redirection(&tok,
-						tok));
-		// word
-		else if (tok->kind == TK_STRING)
-		{
-			cmd->nargs++;
-			cmd->args = add_node_back(cmd->args, word(&tok, tok));
-		}
-		// num
-		else if (tok->kind == TK_NUM)
-		{
-			cmd->nargs++;
-			cmd->args = add_node_back(cmd->args, num(&tok, tok));
-		}
-		// syntax error
-		else
-			syntax_error("Invalid token for <command_element>", &tok, tok);
-	}
+	append_element(cmd, simple_command_element(&tok, tok));
+	while (!at_eof(tok) && !is_control_op(tok))
+		append_element(cmd, simple_command_element(&tok, tok));
 	*rest = tok;
 	return (cmd);
 }
@@ -71,4 +58,47 @@ static bool	is_redirection(const t_token *tok)
 	// num '<<' word
 	// ^ This is invalid
 	return (false);
+}
+
+// simple_command_element = redirection
+//                        | word
+static t_node	*simple_command_element(t_token **rest, t_token *tok)
+{
+	// redirection
+	if (is_redirection(tok))
+		return (redirection(rest, tok));
+	// word
+	else if (tok->kind == TK_STRING)
+		return (word(rest, tok));
+	// num
+	else if (tok->kind == TK_NUM)
+		return (num(rest, tok));
+	// syntax error
+	else
+	{
+		syntax_error("Invalid token for <command_element>", rest, tok);
+		return (NULL);
+	}
+}
+
+bool	is_control_op(t_token *tok)
+{
+	//return (tok->kind == TK_OP && is_control_operator(tok->pos));
+	return (is_control_operator(tok->pos));
+}
+
+static void	append_element(t_node *cmd, t_node *elem)
+{
+	if (elem == NULL)
+		return ;
+	if (elem->kind == ND_WORD || elem->kind == ND_NUM)
+	{
+		cmd->nargs++;
+		cmd->args = add_node_back(cmd->args, elem);
+	}
+	else if (elem->kind == ND_REDIR_IN || elem->kind == ND_REDIR_OUT
+			|| elem->kind == ND_REDIR_APPEND || elem->kind == ND_REDIR_HEREDOC)
+	{
+		cmd->redirects = add_node_back(cmd->redirects, elem);
+	}
 }
